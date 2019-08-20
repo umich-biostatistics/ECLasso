2ee8d11eee0ef6490868219d88c6cdfc0294d33e
Kleinsasser <mkleinsa@UMICH.EDU>
2019-08-20 14:32:31 -0400

    initial commit

---
+++ R/addalpha.R
@@ -0,0 +1,8 @@
+##credit to Nick Gotelli at https://gotellilab.github.io/GotelliLabMeetingHacks/NickGotelli/ColorPalettes.html
+addalpha<-function(colors, alpha=1.0) {
+  r <- col2rgb(colors, alpha=T)
+  # Apply alpha
+  r[4,] <- alpha*255
+  r <- r/255.0
+  return(rgb(r[1,], r[2,], r[3,], r[4,]))
+}
